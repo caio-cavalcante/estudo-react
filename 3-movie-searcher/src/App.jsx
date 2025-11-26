95e6a2bac30b8dd081377b183831041023edd1b0
caio <caio.cav.ara@gmail.com>
2025-11-26 16:19:43 -0300

    Update API integration and implement the search input and results

--- 3-movie-searcher/src/App.jsx
+++ 3-movie-searcher/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react';
 import MovieCard from './MovieCard';
 import './App.css';
 
-const apiUrl = import.meta.env.OMDB_API_URL;
+// secutiry by using dotenv to hide API key and base URL
+const apiKey = import.meta.env.VITE_API_KEY;
+const baseUrl = import.meta.env.VITE_BASE_URL;
 
 const App = () => {
+    // states are being used to store data and update the UI
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
 
+    // the searchMovies function is being used to fetch data from the API and update the state
     const searchMovies = async (title) => {
-        const res = await fetch(`%{OMDB_API_URL}&s=${title}`);
+        const res = await fetch(`${baseUrl}?apiKey=${apiKey}&s=${title}`);
         const data = await res.json();
 
         if(data.Search) {
@@ -18,4 +22,40 @@ const App = () => {
             console.error(data.Error);
         }
     };
+    
+    return (
+        <div className="app">
+            <h1>Movie Lost & Found</h1>
+
+            <div className="search">
+                <input 
+                    placeholder='Search for a movie' // placeholder is being used to give a hint to the user
+                    value={searchTerm} // the value of the input will be used to update the state
+                    onChange={(e) => setSearchTerm(e.target.value)} // the onChange 'tells' react to update the state and setSearchTerm updates the state with the variable from the user's input
+                />
+
+                <button onClick={() => searchMovies(searchTerm)}>
+                    Search
+                </button>
+            </div>
+
+            {/* to render the list, the map function is being used as a 'for each' loop 
+            if there is a movie set in the state, the map will create a new component of MovieCard for each movie in the JSON response
+            */}
+            {movies?.length > 0 ? (
+                <div className="movie-container">
+                    {movies.map((movie) => (
+                        <MovieCard key={movie.imdbID} movie={movie}></MovieCard>
+                    ))}
+                </div>
+            ) : (
+                <div className="empty-container">
+                    <h2>No movies found.</h2>
+                </div>
+            )}
+            {/* if there are no movies in the state, the message will be displayed */}
+        </div>
+    );
 };
+
+export default App;
